13e05f7226a18fa0d201d18f2acd54b712edfe89
Дмитрий <mitya.kulik@bk.ru>
2022-07-03 11:56:10 +0300

    Оба файла

---
+++ code/Program.cs
@@ -0,0 +1,15 @@
+﻿void Main (string[] args)
+{
+    System.Console.WriteLine("Введите предложение");
+    string idea = Console.ReadLine();
+    string[] word = idea.Split(' ', '.', '!', '?');
+    for (int i = 0; i < word.Count(); i++)
+    {
+        if (word[i].Length <= 3)
+        {
+            Console.Write($"{word[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+Main(args);
